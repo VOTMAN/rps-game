0a5135a4b088ceca3fb1f59d45c4fc5dcc2e3d2d
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-08-18 09:39:03 +0530

    Add condition to playRound func and add condition for invalid input by user

--- script.js
+++ script.js
@@ -13,12 +13,24 @@ function playRound(playerSelection, computerSelection) {
 playerSelection = prompt("Enter your choice (Rock, Paper, Scissors)").toLowerCase();
 computerSelection  = getComputerChoice().toLowerCase();
 
+if (playerSelection != choices.toLowerCase()) {
+    console.log("Invalid Input by Player")
+} 
+
 if (playerSelection == computerSelection) {
         console.log("Tie");
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "Rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper") 
+    ) {
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+        // playerScore++
+    } else {
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+        // computerScore++
     }
 }
-   
-
-  
+     
 console.log(playRound(playerSelection, computerSelection));
   
