38337a2477ff2db8bab780555ec9abd2f13530ee
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-08-18 10:27:35 +0530

    Add game func that loops 5 times through playRound
    Log each round result and counts the score to give to output if won or lost with score

--- script.js
+++ script.js
@@ -5,32 +5,55 @@ function getComputerChoice() {
     return choices[compChoice]
 }
 
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 let playerSelection;
 let computerSelection;
 
 function playRound(playerSelection, computerSelection) {
 
 playerSelection = prompt("Enter your choice (Rock, Paper, Scissors)").toLowerCase();
+// console.log(playerSelection);
 computerSelection  = getComputerChoice().toLowerCase();
-
-if (playerSelection != choices.toLowerCase()) {
-    console.log("Invalid Input by Player")
-} 
+// console.log(computerSelection);
 
 if (playerSelection == computerSelection) {
-        console.log("Tie");
+        return ("Tie");
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "Rock") || 
+        (playerSelection == "paper" && computerSelection == "rock") || 
         (playerSelection == "scissors" && computerSelection == "paper") 
     ) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        // playerScore++
+        return (`You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`)
+        
     } else {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
-        // computerScore++
+        return (`You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
     }
 }
-     
-console.log(playRound(playerSelection, computerSelection));
-  
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let roundResult = playRound(playerSelection, computerSelection)
+        
+        if (roundResult.includes("Win")) {
+            console.log(roundResult)
+            playerScore++
+        } else if (roundResult.includes("Lose")) {
+            console.log(roundResult)
+            computerScore++
+        } else {
+            console.log(roundResult)
+        }
+    }
+    if (playerScore >= 3) {
+        console.log(`You Won!, you got ${playerScore} points!`)
+    } else {
+        console.log(`You Lost!, you got ${playerScore} points!`)
+    }
+}
+
+game()
