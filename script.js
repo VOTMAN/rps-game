ccaa7b3bf12c3a347f954d555e660b3b3eefc0cb
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-08-18 09:29:44 +0530

    Declare playRound func to play 1 round

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function getComputerChoice() {
 let playerSelection;
 let computerSelection;
 
+function playRound(playerSelection, computerSelection) {
+
+playerSelection = prompt("Enter your choice (Rock, Paper, Scissors)").toLowerCase();
+computerSelection  = getComputerChoice().toLowerCase();
+
+if (playerSelection == computerSelection) {
+        console.log("Tie");
+    }
+}
+   
+
+  
+console.log(playRound(playerSelection, computerSelection));
+  
