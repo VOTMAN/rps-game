e443df7c61156a4b95fe60dbb20c4a8a7ae96e94
Arnav <76613041+VOTMAN@users.noreply.github.com>
2023-09-03 12:46:22 +0530

    Made rps game that works

--- script.js
+++ script.js
@@ -1,60 +1,79 @@
-let choices = ["Rock", "Paper", "Scissors"]
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll("input");
+const body = document.getElementById("results")
+const roundResult = document.createElement("div");
+const finalResult = document.createElement("div");
+const div = document.createElement("div");
+const score = document.createElement("div")
+
+
+body.appendChild(score);
+body.appendChild(roundResult);
+body.appendChild(finalResult);
+body.appendChild(div)
+
+
+let choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * choices.length)
-    return choices[compChoice]
+    let compChoice = Math.floor(Math.random() * choices.length);
+    return choices[compChoice];
+}
+
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    })
+    finalResult.innerHTML = (`${final}`);
+    finalResult.c
+    div.textContent = ("\nReload to play again!");
 }
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-let playerSelection;
-let computerSelection;
-
-function playRound(playerSelection, computerSelection) {
-
-playerSelection = prompt("Enter your choice (Rock, Paper, Scissors)").toLowerCase();
-computerSelection  = getComputerChoice().toLowerCase();
-
-if (playerSelection == computerSelection) {
-        return ("Tie");
+function playRound(playerSelection) {
+    
+    let computerSelection  = getComputerChoice().toLowerCase();
+    let result = "";
+    let finalResult = "";
+    
+    if (playerSelection == computerSelection) {
+        result = ("Tie");
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "paper" && computerSelection == "rock") || 
         (playerSelection == "scissors" && computerSelection == "paper") 
-    ) {
-        return (`You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`)
-        
-    } else {
-        return (`You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
-    }
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let roundResult = playRound(playerSelection, computerSelection)
-        
-        if (roundResult.includes("Win")) {
-            console.log(roundResult)
+        ) {
+            result = (`Your Point! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`)
             playerScore++
-        } else if (roundResult.includes("Lose")) {
-            console.log(roundResult)
-            computerScore++
-        } else {
-            console.log(roundResult)
+            score.textContent = `Your Score: ${playerScore}\nComputer Score: ${computerScore}`
+            if (playerScore == 5) {
+                final = "You Won :) Congrats!"
+                finalResult.textContent = final;
+                disableButtons()
+            } 
+    } else  {
+        result = (`Computer's Point!! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`)
+        computerScore++
+        score.textContent = `Your Score: ${playerScore}\nComputer Score: ${computerScore}`
+        if (computerScore == 5) {
+            final = ("You Lost :( Better luck next time")
+            disableButtons()
         }
     }
-    if (playerScore == computerScore) {
-        console.log("It's a tie!")
-    }
-    else if (playerScore > computerScore) {
-        console.log(`You Won!, you got ${playerScore} points!`)
-    } else {
-        console.log(`You Lost!, you got ${playerScore} points!`)
-    }
+    roundResult.textContent = result;
+    
 }
 
-game()
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function (){
+        playRound(button.value)
+    })
+});
+
